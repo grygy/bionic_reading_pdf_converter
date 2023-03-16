445c11cf92dee969c5c85ee334c0e7a56ede7ce5
Adam Grygar <grygarad@gmail.com>
2023-03-16 12:59:26 +0000

    Inital versioning commit

---
+++ pdf_converter.py
@@ -0,0 +1,42 @@
+import re
+import sys
+import os
+from bs4 import BeautifulSoup
+import minify_html
+
+
+def convert_pdf_to_html(pdf_file_path, html_folder):
+    os.system(f'mkdir .{html_folder}')
+    os.system(
+        f'pdftohtml -c -noframes {pdf_file_path} .{html_folder}/{html_folder}.html')
+
+
+def hydrate_html_with_bionic(html_folder):
+    with open(f'.{html_folder}/{html_folder}.html', "rb") as original_file:
+        soup = BeautifulSoup(original_file, 'html.parser')
+        spans = soup.find_all('span')
+        for span in spans:
+            words = span.get_text().split()
+            for i in range(len(words)):
+                if re.match('([a-z]|[A-Z])', words[i][0]) != None:
+                    middle = len(words[i]) // 2
+                    words[i] = f'<strong>{words[i][:middle]}</strong>{words[i][middle:]}</span>'
+            new_text = " ".join(words)
+            span.string = ""
+            tmp = BeautifulSoup(new_text, 'html.parser')
+            span.append(tmp)
+
+        # minified = minify_html.minify(code=soup.prettify("utf-8"), remove_processing_instructions=True)
+        with open(f'.{html_folder}/{html_folder}_hydrated.html', "wb") as f_output:
+            f_output.write(soup.prettify("utf-8"))
+
+
+def convert_pdf(filename):
+    html_folder = filename.split('.')[0]
+    convert_pdf_to_html(filename, html_folder)
+    hydrate_html_with_bionic(html_folder)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    convert_pdf(args[0])
