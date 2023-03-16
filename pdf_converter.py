b3b300c6f3066c2b4bc28931408ee5724c8c13bf
Adam Grygar <grygarad@gmail.com>
2023-03-16 17:40:24 +0000

    Add tweak for one letter words

--- pdf_converter.py
+++ pdf_converter.py
@@ -20,6 +20,8 @@ def hydrate_html_with_bionic(html_folder):
             for i in range(len(words)):
                 if re.match(r'([a-z]|[A-Z])', words[i][0]) != None:
                     middle = len(words[i]) // 2
+                    if middle == 0 and len(words[i]) == 1:
+                        middle = 1
                     words[i] = f'<strong>{words[i][:middle]}</strong>{words[i][middle:]}</span>'
             new_text = " ".join(words)
             span.string = ""
@@ -29,12 +31,14 @@ def hydrate_html_with_bionic(html_folder):
         minified = minify_html.minify(
             code=soup.prettify(), remove_processing_instructions=True)
         minified = re.sub(r'(\s<\/strong>\s)', '</strong>', minified)
+        minified = minified.replace('#A0A0A0', '#ffffff', 1)
         with open(f'.{html_folder}/{html_folder}_hydrated.html', "wb") as f_output:
             f_output.write(minified.encode('utf-8'))
 
 
 def convert_html_to_pdf(html_folder):
-    os.system(f'wkhtmltopdf -T 0 -B 0 -L 0 -R 0 --enable-local-file-access --zoom 1.2 .{html_folder}/{html_folder}_hydrated.html {html_folder}_bionic.pdf')
+    os.system(
+        f'wkhtmltopdf -T 0 -B 0 -L 0 -R 0 --enable-local-file-access --zoom 1.2 .{html_folder}/{html_folder}_hydrated.html {html_folder}_bionic.pdf')
 
 
 def convert_pdf(filename):
